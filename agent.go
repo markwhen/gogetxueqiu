5dd3f2450ad32e7730f793aecf06c4a57be0e2c8
Maojia Sheng <markwhat@markwhat.com>
2016-04-23 21:06:02 +0800

    now it got correct json data

--- agent.go
+++ agent.go
@@ -8,12 +8,12 @@ import (
 	"net/http/cookiejar"
 	"net/url"
 	"strings"
-	"fmt"
+	"compress/gzip"
 )
 
 var myCookieJar *cookiejar.Jar
 
-// HTTPGet : request using GET method
+// HTTPGet : request using GET method, will encode the get params into url
 func HTTPGet(urlBase string, params map[string]string) (int, string, error) {
 	url := urlBase
 	if params != nil && len(params) > 0 {
@@ -26,19 +26,19 @@ func HTTPGet(urlBase string, params map[string]string) (int, string, error) {
 	return httpRequest("GET", url, nil)
 }
 
-// HTTPPost : request using POST method
+// HTTPPost : request using POST method, will encode the post params into formBody
 func HTTPPost(urlBase string, params map[string]string) (int, string, error) {
 	val := url.Values{}
 	if params != nil && len(params) > 0 {
 		for k, v := range params {
 			val.Set(k, v)
 		}
-
 	}
 	formBody := ioutil.NopCloser(strings.NewReader(val.Encode()))
 	return httpRequest("POST", urlBase, formBody)
 }
 
+// httpRequest : default charset UTF-8
 func httpRequest(method string, urlStr string, postBody io.ReadCloser) (int, string, error) {
 	if myCookieJar == nil {
 		myCookieJar, _ = cookiejar.New(nil)
@@ -55,14 +55,12 @@ func httpRequest(method string, urlStr string, postBody io.ReadCloser) (int, str
 	if debugLogging {
 		log.Println("httpRequest ", method, " ", urlStr, " ")
 	}
-	req.Header.Set("Host", "xueqiu.com")
 	req.Header.Set("Accept", "*/*")
 	req.Header.Set("X-Requested-With", "XMLHttpRequest")
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36")
 	req.Header.Set("Accept-Encoding", "gzip,deflate,sdch")
 	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.8")
 	req.Header.Set("Connection", "keep-alive")
-	req.Header.Set("Referer", "https://xueqiu.com/")
 
 	if postBody != nil {
 		req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
@@ -75,10 +73,14 @@ func httpRequest(method string, urlStr string, postBody io.ReadCloser) (int, str
 		}
 		return -1, "", err
 	}
-	body, _ := ioutil.ReadAll(res.Body)
-	
-	if debugLogging {
-		log.Println(res.StatusCode, res.Header, res.Body)
+	// ungzip or not
+	var body []byte
+	if res.Header.Get("Content-Encoding") == "gzip" {
+		gzipReader, _ := gzip.NewReader(res.Body)
+		defer gzipReader.Close()
+		body, _ = ioutil.ReadAll(gzipReader)
+	} else {
+		body, _ = ioutil.ReadAll(res.Body)
 	}
-	return res.StatusCode, string(body), nil
+	return res.StatusCode, (string(body)), nil
 }
--- gogetxueqiu.go
+++ gogetxueqiu.go
@@ -8,11 +8,11 @@ var debugLogging = false
 var infoLogging = true
 
 func main() {
-	username, err := Login()
+	_, err := Login()
 	if err != nil {
 		panic("login failed")
 	}
-	fmt.Println(username)
+
 	content, err := StockCurr("SZ000625")
 	if err != nil {
 		fmt.Println(err)
